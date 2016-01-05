8a29607341187039a59b0940f7525a3f5aea7964
Manuel Leduc <manuel.leduc@inria.fr>
2016-01-05 14:09:12 +0100

    cleanup

--- src/main/java/fr/mleduc/docker/kevoree/gateway/Startup.java
+++ src/main/java/fr/mleduc/docker/kevoree/gateway/Startup.java
@@ -1,10 +1,8 @@
 package fr.mleduc.docker.kevoree.gateway;
 
-import org.KevoreeModel;
 import org.kevoree.modeling.KCallback;
 import org.kevoree.modeling.cdn.KContentDeliveryDriver;
 import org.kevoree.modeling.drivers.mongodb.MongoDbContentDeliveryDriver;
-import org.kevoree.modeling.memory.manager.DataManagerBuilder;
 import org.kevoree.modeling.plugin.WebSocketGateway;
 
 import java.net.UnknownHostException;
@@ -14,10 +12,10 @@ import java.net.UnknownHostException;
  */
 public class Startup {
     public static void main(String[] args) throws UnknownHostException {
-        final KContentDeliveryDriver cdn = new MongoDbContentDeliveryDriver("localhost", 27017, "test");
+        final KContentDeliveryDriver cdn = new MongoDbContentDeliveryDriver("localhost", 27017, System.getenv("DATABASE"));
         cdn.connect(new KCallback<Throwable>() {
             public void on(Throwable throwable) {
-                WebSocketGateway wsGateway = WebSocketGateway.expose(cdn, 8888);
+                final WebSocketGateway wsGateway = WebSocketGateway.expose(cdn, 8888);
                 wsGateway.start();
             }
         });
